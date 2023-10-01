c8d39c88bf539c7f5be9546ce2bcde57d1b0f3c5
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-02 01:47:11 +0530

    2nd commit

--- Better Rest AI App/ContentView.swift
+++ Better Rest AI App/ContentView.swift
@@ -15,17 +15,21 @@ struct ContentView: View {
     // MARK: Body
     var body: some View {
         VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+//            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+//            
+//            DatePicker("Please pick a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
+//                .labelsHidden()
             
-            DatePicker("Please pick a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
-                .labelsHidden()
+            Text(Date.now, format: .dateTime.year().day().month().hour().minute())
+            Text(Date.now.formatted(date: .long, time: .shortened))
         }
         .padding()
     }
     
-    func exampleDate() {
-        let tommorow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tommorow
+    func trivialExample() {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
