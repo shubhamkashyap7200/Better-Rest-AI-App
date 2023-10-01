27ccbc3a3d3a824fd1293b249efac40b363198bf
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-02 00:49:42 +0530

    Initial Commit

---
+++ Better Rest AI App/Better_Rest_AI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_Rest_AI_AppApp.swift
+//  Better Rest AI App
+//
+//  Created by Shubham on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Better_Rest_AI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
