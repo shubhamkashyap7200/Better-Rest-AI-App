21d952b1db5a8b00921cea7cf1fa3afa4ffa2834
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-02 01:21:50 +0530

    1st Commit

--- Better Rest AI App/ContentView.swift
+++ Better Rest AI App/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Properties
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
+    // MARK: Body
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            
+            DatePicker("Please pick a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
+                .labelsHidden()
         }
         .padding()
+    }
+    
+    func exampleDate() {
+        let tommorow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tommorow
     }
 }
 
