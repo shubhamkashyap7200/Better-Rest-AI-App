a2084fccb33ea1fe7f64ecf5fed78383cdce1595
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-02 10:35:33 +0530

    Third Commit

--- Better Rest AI App/ContentView.swift
+++ Better Rest AI App/ContentView.swift
@@ -11,19 +11,34 @@ struct ContentView: View {
     // MARK: Properties
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     
     // MARK: Body
     var body: some View {
-        VStack {
-//            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-//            
-//            DatePicker("Please pick a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
-//                .labelsHidden()
-            
-            Text(Date.now, format: .dateTime.year().day().month().hour().minute())
-            Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationStack {
+            VStack(alignment: .leading) {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20, step: 1)
+
+            }
+            .padding()
+
+
+            .navigationTitle("Better Rest")
         }
-        .padding()
     }
     
     func trivialExample() {
@@ -36,3 +51,13 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+
+//            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+//
+//            DatePicker("Please pick a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
+//                .labelsHidden()
+//            
+//            Text(Date.now, format: .dateTime.year().day().month().hour().minute())
+//            Text(Date.now.formatted(date: .long, time: .shortened))
